841d1a4e9409ac3dfc4bb170a877d2a1dcdd58f0
Sandor Muranyi <contact@mudlee.hu>
2020-02-17 13:15:23 +0100

    Hydra - Adding links to the legacy explorers

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,24 +1,30 @@
 <template>
-  <footer class="AppFooter px-5 md:px-10 flex-col lg:flex-row">
-    <div class="mb-4 lg:mb-0 text-center md:text-left">
-      <span class="block md:inline-block mb-4 md:mb-0 text-center"
-        >&copy; <a :href="brandData.footer.url" target="_blank">{{ brandData.footer.text }}</a> | {{ year }}.
-        {{ $t("FOOTER.ALL_RIGHTS_RESERVED") }}</span
-      >
-      <div class="block md:inline-block">
-        <span class="hidden md:inline-block">&nbsp;|&nbsp;</span>
-        <span
-          ><!--{{ $t("FOOTER.VERSION") }}: <a :href="versionLink" target="_blank">{{ version }}</a> |-->
-          {{ $t("FOOTER.DATE") }}: {{ date }}</span
+  <div>
+    <footer class="AppFooter px-5 md:px-10 flex-col lg:flex-row">
+      <div class="mb-4 lg:mb-0 text-center md:text-left">
+        <span class="block md:inline-block mb-4 md:mb-0 text-center"
+          >&copy; <a :href="brandData.footer.url" target="_blank">{{ brandData.footer.text }}</a> | {{ year }}.
+          {{ $t("FOOTER.ALL_RIGHTS_RESERVED") }}</span
         >
+        <div class="block md:inline-block">
+          <span class="hidden md:inline-block">&nbsp;|&nbsp;</span>
+          <span
+            ><!--{{ $t("FOOTER.VERSION") }}: <a :href="versionLink" target="_blank">{{ version }}</a> |-->
+            {{ $t("FOOTER.DATE") }}: {{ date }}</span
+          >
+        </div>
       </div>
+      <div class="text-center">
+        <a v-for="social in socialMediaData" :key="social.name" :href="social.url" target="_blank">
+          <SvgIcon class="m-1" :name="`social/${social.name}`" :view-box="social.viewBox" />
+        </a>
+      </div>
+    </footer>
+    <div style="max-width:75rem; margin:auto;font-size:80%;color:#626466;" class="px-10 pb-10 text-center" v-if="isMain">
+      Looking for the legacy IOP network? <a href="https://blockexperts.com/iop" target="_blank">Click Here</a>. 
+      Or looking for the legacy ERC20 token? <a href="https://etherscan.io/token/0xd233495c48eb0143661ffc8458eafc21b633f97f" target="_blank">Click Here</a>
     </div>
-    <div class="text-center">
-      <a v-for="social in socialMediaData" :key="social.name" :href="social.url" target="_blank">
-        <SvgIcon class="m-1" :name="`social/${social.name}`" :view-box="social.viewBox" />
-      </a>
-    </div>
-  </footer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -50,6 +56,11 @@ export default class AppFooter extends Vue {
 
   get socialMediaData() {
     return socialMedia;
+  }
+
+  get isMain(): boolean {
+    const network = require(`../../networks/${process.env.VUE_APP_EXPLORER_CONFIG}`);
+    return network.alias === "Main";
   }
 }
 </script>
